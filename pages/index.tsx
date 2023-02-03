8e4b1825d81b2251efc47f147de64c8506bbccf2
George Saint Hilaire <georgesainthilaire@Georges-iMac.local>
2023-02-03 12:26:02 -0500

    Adding dark mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,13 +24,15 @@ export default function Home() {
       </Head>
       <main className="bg-white px-10">
         <section>
-          <nav className='py-10 flex justify-between'>
-            <h1 className='text-xl font-burtons'>George Saint Hilaire SWE</h1>
-            <ul className='flex items-center'>
-              <li><BsFillMoonStarsFill className='cursor-pointer text-2xl '/></li>
-              <li><a className="bg-gradient-to-r from-blue-500 to-teal-500 text-white px-4 py-2 rounded ml-8" href="#">Resume</a></li>
-            </ul>
-          </nav>
+          <div className=''>
+            <nav className='py-10 flex justify-between '>
+              <h1 className='text-xl font-burtons'>George Saint Hilaire SWE</h1>
+              <ul className='flex items-center'>
+                <li><BsFillMoonStarsFill className='cursor-pointer text-2xl '/></li>
+                <li><a className="bg-gradient-to-r from-blue-500 to-teal-500 text-white px-4 py-2 rounded ml-8" href="#">Resume</a></li>
+              </ul>
+            </nav>
+          </div>
           <div className="flex items-center bg-gradient-to-t from-blue-500 to-teal-500 rounded-t-2xl p-5 ">
               <div className="text-center p-10 ">
                 <h2 className="text-5xl py-2 text-purple-600 ">George Saint Hilaire</h2>
@@ -70,13 +72,13 @@ export default function Home() {
               and collaborated with talanted people to create digital products
               for both business and consumer use.
             </p>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+            <p className="text-lg  py-2 leading-8 text-gray-800 dark:text-gray-200">
               I offer from a wide range of services, including brand design,
               programming and teaching.
             </p>
           </div>
           <div className="lg:flex gap-10">
-            <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 bg-teal-500 flex-1">
               <Image src={design} alt="images"
                width={100} height={100} />
               <h3 className="text-lg font-medium pt-8 pb-2  ">
@@ -86,13 +88,13 @@ export default function Home() {
                 Creating elegant designs suited for your needs following core
                 design theory.
               </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
+              <h4 className="py-4 font-bold text-black-600">Design Tools I Use</h4>
               <p className="text-gray-800 py-1">Photoshop</p>
               <p className="text-gray-800 py-1">Illustrator</p>
               <p className="text-gray-800 py-1">Figma</p>
               <p className="text-gray-800 py-1">Indesign</p>
             </div>
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 bg-blue-500 flex-1">
               <Image src={code}  alt="images"
               width={100} height={100} />
               <h3 className="text-lg font-medium pt-8 pb-2 ">
@@ -102,13 +104,13 @@ export default function Home() {
                 Do you have an idea for your next great website? Let's make it a
                 reality.
               </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
+              <h4 className="py-4 font-bold text-black-600">Design Tools I Use</h4>
               <p className="text-gray-800 py-1">Photoshop</p>
               <p className="text-gray-800 py-1">Illustrator</p>
               <p className="text-gray-800 py-1">Figma</p>
               <p className="text-gray-800 py-1">Indesign</p>
             </div>
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 bg-purple-500 flex-1">
               <Image src={consulting} alt="images"
                width={100} height={100} />
               <h3 className="text-lg font-medium pt-8 pb-2 ">Consulting</h3>
@@ -116,7 +118,7 @@ export default function Home() {
                 Are you interested in feedback for your current project? I can
                 give you tips and tricks to level it up.
               </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
+              <h4 className="py-4 font-bold text-black-600">Design Tools I Use</h4>
               <p className="text-gray-800 py-1">Photoshop</p>
               <p className="text-gray-800 py-1">Illustrator</p>
               <p className="text-gray-800 py-1">Figma</p>
@@ -125,7 +127,7 @@ export default function Home() {
           </div>
         </section>
         <section className="py-10">
-          <div className=''>
+          <div className='text-center py-12'>
             <h3 className="text-3xl py-1 dark:text-white ">Portofolio</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
               Since the beginning of my journey as a freelance designer and
